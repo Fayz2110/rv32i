676214a730e0b33ce813088dfd0a9395603f2e51
Muddassir Ali <https://github.com/Ali-975>
2024-03-06 14:08:17 +0500

    update the top and testbenches

--- src/register.v
+++ src/register.v
@@ -1,23 +1,40 @@
 module register(oprs1,oprs2,oprd,wrt_en,rst,clk,rs1,rs2,wrt_data);
+input wire clk;
 input [31:0]wrt_data;
 input [4:0]oprs1,oprs2,oprd;
-input clk;
 input wrt_en,rst;
 output reg [31:0] rs1,rs2;
 
-reg [31:0] register [0:31];
+reg [31:0] register [31:0];
+
+integer i;
 
 always @(oprs1 )begin
-    rs1=register[oprs1];
+    // rs1[0] = 0;
+    register[0] = 0;
+    register[oprs1] = rs1;
 end
 
 always @(oprs2 )begin
-    rs2=register[oprs2];
+    // rs2[0] = 0;
+    register[0] = 0;
+    register[oprs2] = rs2;
 end
 always @(posedge clk)begin
-    if(wrt_en & !rst)begin
-        register[oprd]<=wrt_data;
-    end
+    if(rst)begin
+        for (i = 0; i <= 31; i++) begin
+            register[i] = 0;
+        end
+    end else begin
+        register[oprd] = wrt_data;
+        end
 end
+    // assign rs1 = (oprs1 != 0) ? register[oprs1] : 0;    // if reg[0] = 0
+    // assign rs2 = (oprs2 != 0) ? register[oprs2] : 0;    // if reg[0] = 0
+// always @(posedge clk)begin
+//     if(wrt_en & !rst)begin
+//         register[oprd]<=wrt_data;
+//     end
+// end
 
 endmodule
--- src/top.v
+++ src/top.v
@@ -1,5 +1,6 @@
 `include "decode.v"
 `include "register.v"
+
 module top(rst);
 reg [31:0]ins;
 reg clk;
@@ -13,15 +14,13 @@ input rst;
 
 wire [31:0] rs1,rs2;
 
-
 decode dut_dec(
     .ins(ins),
     .oprs1(oprs1),
     .oprs2(oprs2),
     .oprd(oprd),
     .aluop(aluop),
-    .imm(imm),
-    .wrt_en(wrt_en),  
+    .imm(imm)
 );
 
 register reg_dut_inst(
@@ -36,21 +35,4 @@ register reg_dut_inst(
     .rs2(rs2)
 );
 
-always #10 clk = ~clk;
-
-initial begin
-    clk=0;
-    ins=32'hff700293;
-    #20
-    ins=32'h00700313;
-    #20
-    ins=32'h40628233;
-
-    #80 $finish;
-end
-
-initial begin
-$dumpfile("top.vcd");
-$dumpvars(0, top);
-end
 endmodule
